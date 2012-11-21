9f01988d5fdba658ba92bc530fe7a83d197f396e
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-20 19:27:10 -0600

    Add skeleton files for shell

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='conch',
+    version='0.0.1',
+    author="Evan Driscoll",
+    author_email="driscoll@cs.wisc.edu",
+    maintainer="Evan Driscoll",
+    maintainer_email="driscoll@cs.wisc.edu",
+    packages=["conch"],
+    package_dir = {'': 'src'},
+    license = "GPL3",
+    )
