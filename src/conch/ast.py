6e1bdb63ba9b73ae6f8f33a6016a90cc8bbc2295
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-22 13:42:26 -0600

    Added Word AST class and some tests

---
+++ src/conch/ast.py
@@ -0,0 +1,6 @@
+class Word(object):
+    def __init__(self, word):
+        self._word = word
+
+    def getWord(self):
+        return self._word
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,9 +1,26 @@
 import unittest
-import conch
+import conch.ast
 
-class TestLexer(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual(1,1)
+ast = conch.ast
+
+#class Test_Position(unittest.TestCase):
+#    def test_create(self):
+#        p = ast.Position(1,2)
+
+class Test_Word(unittest.TestCase):
+    def test_create(self):
+        w = ast.Word("a")
+
+    def test_retrieve_a_word(self):
+        w = ast.Word("a")
+        w.getWord()
+
+    def test_retrieve_original_word(self):
+        w1 = ast.Word("a1")
+        w2 = ast.Word("a2")
+        self.assertEqual("a1", w1.getWord())
+        self.assertEqual("a2", w2.getWord())
+
 
 if __name__ == '__main__':
     unittest.main()
