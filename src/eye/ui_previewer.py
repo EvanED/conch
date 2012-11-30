e21d6f8f0c8f013dbf9530632bef6de7523e286f
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-30 10:08:08 -0600

    Remove the cleare & preview buttons

--- src/eye/ui_previewer.py
+++ src/eye/ui_previewer.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'previewer.ui'
 #
-# Created: Mon Nov 29 17:09:55 2010
-#      by: PyQt4 UI code generator snapshot-4.8.2-241fbaf4620d
+# Created: Fri Nov 30 10:07:50 2012
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,7 +17,7 @@ except AttributeError:
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(911, 688)
+        Form.resize(566, 169)
         self.horizontalLayout_4 = QtGui.QHBoxLayout(Form)
         self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
         self.splitter = QtGui.QSplitter(Form)
@@ -32,15 +32,6 @@ class Ui_Form(object):
         self.plainTextEdit = QtGui.QPlainTextEdit(self.editorBox)
         self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
         self.verticalLayout_2.addWidget(self.plainTextEdit)
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.clearButton = QtGui.QPushButton(self.editorBox)
-        self.clearButton.setObjectName(_fromUtf8("clearButton"))
-        self.horizontalLayout.addWidget(self.clearButton)
-        self.previewButton = QtGui.QPushButton(self.editorBox)
-        self.previewButton.setObjectName(_fromUtf8("previewButton"))
-        self.horizontalLayout.addWidget(self.previewButton)
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.horizontalLayout_2.addLayout(self.verticalLayout_2)
         self.previewerBox = QtGui.QGroupBox(self.splitter)
         self.previewerBox.setObjectName(_fromUtf8("previewerBox"))
@@ -53,14 +44,11 @@ class Ui_Form(object):
         self.horizontalLayout_4.addWidget(self.splitter)
 
         self.retranslateUi(Form)
-        QtCore.QObject.connect(self.clearButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.plainTextEdit.clear)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
         self.editorBox.setTitle(QtGui.QApplication.translate("Form", "HTML Editor", None, QtGui.QApplication.UnicodeUTF8))
-        self.clearButton.setText(QtGui.QApplication.translate("Form", "Clear", None, QtGui.QApplication.UnicodeUTF8))
-        self.previewButton.setText(QtGui.QApplication.translate("Form", "Preview", None, QtGui.QApplication.UnicodeUTF8))
         self.previewerBox.setTitle(QtGui.QApplication.translate("Form", "HTML Preview", None, QtGui.QApplication.UnicodeUTF8))
 
 from PyQt4 import QtWebKit
