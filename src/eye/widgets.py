2ffabde1440674b130880632a93e8bca1f01fa5c
Evan Driscoll <driscoll@cs.wisc.edu>
2012-12-05 09:50:58 -0600

    Minor refactoring: save super(...) in self.my_super

--- src/eye/widgets.py
+++ src/eye/widgets.py
@@ -3,9 +3,14 @@ from PyQt4 import QtCore, QtGui
 class EnterActionTextEdit(QtGui.QPlainTextEdit):
     returnPressed = QtCore.pyqtSignal('QString')
 
+    def __init__(self, parent=None):
+        my_super = super(EnterActionTextEdit, self)
+        my_super.__init__(parent)
+        self._my_super = my_super
+
     def keyPressEvent(self, event):
         if (event.key() == QtCore.Qt.Key_Return):
             self.returnPressed.emit(self.toPlainText())
         else:
-            super(EnterActionTextEdit, self).keyPressEvent(event)
+            self._my_super.keyPressEvent(event)
 
