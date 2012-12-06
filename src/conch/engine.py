d7ef2ae576dad9a8f272078ab2158a245b7a2fb1
Evan Driscoll <driscoll@cs.wisc.edu>
2012-12-06 10:00:17 -0600

    Change to using the pty module with explicit fork/exec

--- src/conch/engine.py
+++ src/conch/engine.py
@@ -1,10 +1,19 @@
 import subprocess
+import pty
 
+class Process(object):
+    def __init__(self, stdin, stdout):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stdout #??
 
 def execute(ast):
     args = ast.get_argv()
-    child = subprocess.Popen(args,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             stdin=subprocess.PIPE)
-    return child
+    (child_pid, master_fd) = pty.fork()
+    if child_pid == 0:
+        # Child. master_fd is invalid
+        os.execvp(args[0], args)
+    stdout = os.fdopen(master_fd, "r", 0)
+    stdin = os.fdopen(master_fd, "w", 0)
+
+    return Process(stdin, stdout)
